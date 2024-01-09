6525cfbc6c252b555ebf18c2e62332266875fd2d
nickonyi <nickoluoch1130@gmail.com>
2024-01-09 06:29:52 +0300

    codes

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,21 +3,39 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+function List(props){
+  
+  if(!props.animals){
+     return <div>Loading...</div>
+  }
+  
+  if(props.animals.length === 0){
+    return <div>There is nothing in the list!!!</div>
+  }
+  return (
+    
+      <ul>
+        {
+          props.animals.map(animal=>{
+            return animal.startsWith("L")? <li key={animal}>{animal}</li>:null;
+          })
+        }
+      </ul>
+  
+  )
+}
+
 function App() {
 
-const animals = ["Lion","Snake","Lizard","Gazelle","Buffaloe"];
+const animals = [];
 
 
   return (
     <div>
       <h1>Animals:</h1>
-      <ul>
-        {
-        animals.map((animal)=>{
-        return <li key={animal}>{animal}</li>
-        })
-        }
-      </ul>
+       <List animals={animals}/>
     </div>
   )
 }
