dfa1bf5e14724a360fff7eeaae8618c296865fa7
nickonyi <nickoluoch1130@gmail.com>
2024-01-09 07:33:55 +0300

    code

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -7,22 +7,20 @@ import './App.css'
 
 function List(props){
   
-  if(!props.animals){
-     return <div>Loading...</div>
-  }
-  
-  if(props.animals.length === 0){
-    return <div>There is nothing in the list!!!</div>
-  }
   return (
-    
-      <ul>
-        {
-          props.animals.map(animal=>{
-            return animal.startsWith("L")? <li key={animal}>{animal}</li>:null;
-          })
-        }
-      </ul>
+     <>
+       {!props.animals && <div>Loading...</div>}
+       {props.animals && props.animals.length>0&& (
+        <ul>
+          {
+            props.animals.map(animal=>{
+              return <li key={animal}>{animal}</li>
+            })
+          }
+        </ul>
+       )}
+       {props.animals && props.animals.length === 0 && <div>There are no animals in the list!!</div>}
+     </>
   
   )
 }
